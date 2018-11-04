8d13c4c9ad26c173dca84cab9da343ec107bcef1
rbrick <rbrickproductions123@gmail.com>
2018-11-03 22:19:14 -0500

    start work on bungeecord plugin

---
+++ simple2fa-bungee/src/main/kotlin/io/dreamz/simple2fa/Simple2FA.kt
@@ -0,0 +1,29 @@
+package io.dreamz.simple2fa
+
+import net.md_5.bungee.api.event.ServerConnectEvent
+import net.md_5.bungee.api.event.ServerSwitchEvent
+import net.md_5.bungee.api.plugin.Listener
+import net.md_5.bungee.api.plugin.Plugin
+import net.md_5.bungee.event.EventHandler
+
+// TODO: 1.13 has changed plugin messaging names; rename to 'simple2fa:auth'
+
+// The plugin message channel to use
+const val PLUGIN_MESSAGE_CHANNEL = "S2FA|Auth"
+
+class Simple2FA : Plugin() {
+
+    object ConnectListener : Listener {
+        @EventHandler
+        fun onPlayerJoin(event: ServerConnectEvent) = Unit
+
+        @EventHandler
+        fun onServerSwitch(event: ServerSwitchEvent) = Unit
+    }
+
+
+    override fun onEnable() {
+        this.proxy.registerChannel(PLUGIN_MESSAGE_CHANNEL)
+        this.proxy.pluginManager.registerListener(this, ConnectListener)
+    }
+}
